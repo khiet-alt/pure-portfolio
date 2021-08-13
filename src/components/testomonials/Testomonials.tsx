a6911946b3f8e4c3213246b0e7e3c919aff28503
thanh khiet <cs.thanhkhiet@gmail.com>
2021-08-13 21:57:12 +0700

    setup

---
+++ src/components/testomonials/Testomonials.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './testomonial.scss'
+
+export default function Testomonials() {
+    return (
+        <div className="testomonials">
+            
+        </div>
+    )
+}
